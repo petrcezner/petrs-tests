3c6454f4d20976ceb7e15c0c28214a98ea9823a0
Petr Cezner <petr.cezner@datasentics.com>
2023-01-05 08:58:04 +0100

    update calendar

--- gaussian_processes/utils/calendar_tests.py
+++ gaussian_processes/utils/calendar_tests.py
@@ -3,11 +3,13 @@ from holidays import Czechia
 
 cz_holidays = Czechia(years=[2022])
 
-range_of_dates = pd.date_range("2020-01-01", "2022-12-31")
+frequency = 15
+range_of_dates = pd.date_range("2020-01-01", "2022-12-31", freq=f'{frequency}min')
 df = pd.DataFrame(
     index=range_of_dates,
     data={"is_holiday": [date in cz_holidays for date in range_of_dates]}
 )
+df['is_holiday'] = df['is_holiday'].astype(int)
 
 
 
